331376c3e4e80dc831e790e307589e90724e50e3
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-03-27 16:30:25 +0000

    Profile coded styling neede

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,3 +1,9 @@
+import { FaAirbnb } from "react-icons/fa";
+import { GiCubeforce } from "react-icons/gi";
+import { SiYamahamotorcorporation } from "react-icons/si";
+import { SlSocialSoundcloud } from "react-icons/sl";
+import { TbSocial } from "react-icons/tb";
+
 import "./index.css";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
@@ -14,7 +20,8 @@ const Profile = () => {
       <div className="profile-bg">
         <Sidebar />
 
-        <div className="User-profile">
+        <div className="user-profile">
+          <h2 className="greetings">Hello ! 👋</h2>
           <div className="user-details">
             <img src={userPic} alt="profile_pic" className="profile-img" />
             <h3 className="user-name">
@@ -26,17 +33,37 @@ const Profile = () => {
 
           <ul className="user-accounts">
             <li className="account-item">
-              <p className="account-data">Balance: $100.00</p>
+              <p className="account-data">Balance : </p>
+              <span className="amount">$100.00</span>
             </li>
             <li className="account-item">
-              <p className="account-data">Invested: $10.50</p>
+              <p className="account-data">Invested : </p>
+              <span className="amount">$100.00</span>
             </li>
             <li className="account-item">
-              <p className="account-data">Stocks Owned: 5</p>
+              <p className="account-data">Stocks Owned : </p>
+              <span className="amount">$100.00</span>
             </li>
           </ul>
 
-          <div className="investment-list">Render the investment items</div>
+          <p> My Investments</p>
+          <div className="investment-list">
+            <div className="invested-item">
+              <FaAirbnb size="33" color="#fffccf" />
+            </div>
+            <div className="invested-item">
+              <GiCubeforce size="33" color="#fffccf" />
+            </div>
+            <div className="invested-item">
+              <SiYamahamotorcorporation size="33" color="#fffccf" />
+            </div>
+            <div className="invested-item">
+              <SlSocialSoundcloud size="33" color="#fffccf" />
+            </div>
+            <div className="invested-item">
+              <TbSocial size="33" color="#fffccf" />
+            </div>
+          </div>
         </div>
       </div>
     </>
