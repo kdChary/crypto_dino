225788ad3d062d536227397852aa06db0abdd09e
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-03-28 03:53:13 +0000

    Charts added

--- src/components/EmptyPage/index.js
+++ src/components/EmptyPage/index.js
@@ -1,23 +1,27 @@
 import Header from "../Header";
 import "./index.css";
+import Sidebar from "../Sidebar";
 
 const EmptyPage = () => (
   <>
     <Header />
     <div className="empty-page-bg">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-watch-no-saved-videos-img.png"
-        alt="no-data"
-        className="empty-image"
-      />
-      <div className="empty-text-section">
-        <h3 className="empty-heading">You Have No Data To Present.</h3>
-        <p className="empty-description">
-          Please Trade to Update Your Activity...
-        </p>
-        <button type="button" className="empty-btn">
-          Start Trading
-        </button>
+      <Sidebar />
+      <div className="empty-page-contents">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-watch-no-saved-videos-img.png"
+          alt="no-data"
+          className="empty-image"
+        />
+        <div className="empty-text-section">
+          <h3 className="empty-heading">You Have No Data To Present.</h3>
+          <p className="empty-description">
+            Please Trade to Update Your Activity...
+          </p>
+          <button type="button" className="empty-btn">
+            Start Trading
+          </button>
+        </div>
       </div>
     </div>
   </>
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,4 +1,19 @@
 import { Component } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
+
+import { FaBitcoin, FaEthereum } from "react-icons/fa";
+import { SiDogecoin } from "react-icons/si";
+import { SiLitecoin } from "react-icons/si";
 
 import "./index.css";
 import AppContext from "../../context";
@@ -15,12 +30,19 @@ const fetchStatusConst = {
 class Home extends Component {
   state = {
     responseStatus: fetchStatusConst.initial,
-    populationData: {},
+    populationData: [],
   };
 
   componentDidMount() {
     this.getPopulation();
   }
+
+  modifyData = (data) => ({
+    id: data["ID Nation"],
+    year: data["ID Year"],
+    nation: data.Nation,
+    population: data.Population,
+  });
 
   getPopulation = async () => {
     this.setState({ responseStatus: fetchStatusConst.inProgress });
@@ -29,14 +51,110 @@ class Home extends Component {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    if (response.ok) {
+      const modifiedData = data.data.map((item) => this.modifyData(item));
+      this.setState({
+        populationData: modifiedData,
+        responseStatus: fetchStatusConst.success,
+      });
+      //   console.log(modifiedData);
+    } else {
+      this.setState({ responseStatus: fetchStatusConst.failure });
+      console.log("failed to fetch");
+    }
   };
 
-  renderRecharts = () => (
-    <div className="fill-space1">
-      loresum ipseram feoreu afagd as doaeufa vageap
-    </div>
-  );
+  renderRecharts = () => {
+    const { populationData } = this.state;
+
+    const DataFormatter = (number) => {
+      if (number > 1000000) {
+        return `${(number / 1000000).toString()}M`;
+      }
+      return number.toString();
+    };
+
+    return (
+      <ResponsiveContainer width="95%" height={350}>
+        <BarChart
+          data={populationData}
+          margin={{
+            top: 5,
+          }}
+        >
+          <XAxis
+            dataKey="year"
+            tick={{
+              stroke: "gray",
+              strokeWidth: 0.7,
+            }}
+          />
+          <YAxis
+            tickFormatter={DataFormatter}
+            tick={{
+              stroke: "gray",
+              strokeWidth: 0,
+            }}
+          />
+          <Legend
+            wrapperStyle={{
+              padding: 30,
+            }}
+          />
+          <Bar
+            dataKey="population"
+            name="US Population"
+            fill="#1f77b4"
+            barSize="10%"
+          />
+          {/* <Bar dataKey="girls" name="Girls" fill="#fd7f0e" barSize="20%" /> */}
+        </BarChart>
+      </ResponsiveContainer>
+    );
+  };
+
+  renderChartsSm = () => {
+    const data = [
+      {
+        count: 809680,
+        language: "Telugu",
+      },
+      {
+        count: 4555697,
+        language: "Hindi",
+      },
+      {
+        count: 12345657,
+        language: "English",
+      },
+    ];
+    return (
+      <ResponsiveContainer width="100%" height={350}>
+        <PieChart>
+          <Pie
+            cx="50%"
+            cy="40%"
+            data={data}
+            startAngle={0}
+            endAngle={360}
+            innerRadius="40%"
+            outerRadius="70%"
+            dataKey="count"
+          >
+            <Cell name="Dodge" size="13" fill="#fecba6" />
+            <Cell name="Etherium" fill="#b3d23f" />
+            <Cell name="Bitcoin" fill="#a44c9e" />
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="horizontal"
+            verticalAlign="center"
+            align="center"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    );
+  };
 
   render() {
     const { activeTab } = this.state;
@@ -60,7 +178,24 @@ class Home extends Component {
               </button>
             </div>
 
-            {this.renderRecharts()}
+            <div className="hide">{this.renderRecharts()}</div>
+            <div className="show">{this.renderChartsSm()}</div>
+
+            <div className="home-assets">
+              <div className="home-asset-item">
+                <FaBitcoin />
+              </div>
+              <div className="home-asset-item">
+                <FaEthereum />
+              </div>
+              <div className="home-asset-item">
+                <SiDogecoin />
+              </div>
+              <div className="home-asset-item">
+                <SiLitecoin />
+              </div>
+            </div>
+            <p className="info">Navigate to Assets Page for more Info!</p>
           </div>
         </div>
       </>
